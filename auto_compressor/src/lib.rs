bbb54f2267d14eb7db9e1dcaa49c3e90c8ae41e2
Azrenbeth <7782548+Azrenbeth@users.noreply.github.com>
2021-09-14 16:30:56 +0100

    Add pyo3 bindings to run compress_largest_rooms

--- auto_compressor/src/lib.rs
+++ auto_compressor/src/lib.rs
@@ -6,9 +6,13 @@
 //! to the database and uses these to enable it to incrementally work
 //! on space reductions
 
+use anyhow::Result;
+use log::{error, LevelFilter};
+use pyo3::{
+    exceptions::PyRuntimeError, prelude::pymodule, types::PyModule, PyErr, PyResult, Python,
+};
 use std::str::FromStr;
 
-use anyhow::Result;
 use synapse_compress_state::Level;
 
 pub mod manager;
@@ -49,4 +53,71 @@ impl FromStr for LevelInfo {
         // Return the built up vector inside a LevelInfo struct
         Ok(LevelInfo(level_info))
     }
+}
+
+// PyO3 INTERFACE STARTS HERE
+#[pymodule]
+fn auto_compressor(_py: Python, m: &PyModule) -> PyResult<()> {
+    #[pyfn(m, compress_largest_rooms)]
+    fn compress_largest_rooms(
+        py: Python,
+        db_url: String,
+        chunk_size: i64,
+        default_levels: String,
+        number_of_rooms: i64,
+    ) -> PyResult<()> {
+        // Stops the compressor from holding the GIL while running
+        py.allow_threads(|| {
+            compress_largest_rooms_body(db_url, chunk_size, default_levels, number_of_rooms)
+        })
+    }
+
+    fn compress_largest_rooms_body(
+        db_url: String,
+        chunk_size: i64,
+        default_levels: String,
+        number_of_rooms: i64,
+    ) -> PyResult<()> {
+        let _ = pyo3_log::Logger::default()
+            // don't send out anything lower than a warning from other crates
+            .filter(LevelFilter::Warn)
+            // don't log warnings from synapse_compress_state, the auto_compressor handles these
+            // situations and provides better log messages
+            .filter_target("synapse_compress_state".to_owned(), LevelFilter::Error)
+            // log info and above for the auto_compressor
+            .filter_target("auto_compressor".to_owned(), LevelFilter::Info)
+            .install();
+        // ensure any panics produce error messages in the log
+        log_panics::init();
+        // Announce the start of the program to the logs
+        log::info!("auto_compressor started");
+
+        // Parse the default_level string into a LevelInfo struct
+        let default_levels: LevelInfo = match default_levels.parse() {
+            Ok(l_sizes) => l_sizes,
+            Err(e) => {
+                return Err(PyErr::new::<PyRuntimeError, _>(format!(
+                    "Unable to parse level_sizes: {}",
+                    e
+                )))
+            }
+        };
+
+        // call compress_largest_rooms with the arguments supplied
+        let run_result = manager::compress_largest_rooms(
+            &db_url,
+            chunk_size,
+            &default_levels.0,
+            number_of_rooms,
+        );
+
+        if let Err(e) = run_result {
+            error!("{}", e);
+            return Err(PyErr::new::<PyRuntimeError, _>(e.to_string()));
+        }
+
+        log::info!("auto_compressor finished");
+        Ok(())
+    }
+    Ok(())
 }
